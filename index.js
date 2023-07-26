f7c2f9f4927b44e1470ca4b9c90f74dee7d1e367
Valerie Elizabeth <119483866+Valliebby@users.noreply.github.com>
2023-07-26 12:55:41 -0500

    Updated Readme

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const { type } = require('os');
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is your github?',
+        name: 'github',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
